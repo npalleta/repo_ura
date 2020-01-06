cb3e460b5a9f3e9de263b1c5c754f2cea160359a
Niky Lima <palleta.sp@gmail.com>
2020-01-06 09:06:14 -0300

    Commit

---
+++ automacao-ura/automacao-ura-v4/src/test/java/br/com/rsi/ura/suiteTeste/geral/RelatorioGeral.java
@@ -0,0 +1,13 @@
+package br.com.rsi.ura.suiteTeste.geral;
+
+import org.junit.Test;
+
+import br.com.rsi.ura.utils.UtilsUra;
+
+public class RelatorioGeral {
+
+	@Test
+	public void report() {
+		UtilsUra.cmdCommandExecReport();
+	}
+}
